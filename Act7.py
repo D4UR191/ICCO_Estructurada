432a7fe2e1e0edc46e4783c5186fb0cae67fdbd9
D4UR191 <64598563+D4UR191@users.noreply.github.com>
2020-05-21 00:43:59 -0500

    Delete Act7.py

--- Act7.py
+++
@@ -1,9 +0,0 @@
-# Escribir un archivo 2
-
-print("\nNuevo documento")
-texto = input(">>\t")
-nombre = input("Nombre del archivo: ")
-file = open(f"C:/Users/OVN91/Desktop/{nombre}.txt", "w")
-file.write(texto)
-file.close()
-print("\nGuardado")
