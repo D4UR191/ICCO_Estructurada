a47932ef4c75ca8d31460b6847e0e65d7b92ec30
D4UR191 <64598563+D4UR191@users.noreply.github.com>
2020-05-21 00:44:10 -0500

    Delete Act1.py

--- Act1.py
+++
@@ -1,8 +0,0 @@
-# Imprimir números enteros entre n y m
-
-n = 10  # Límite inferior
-m = 20  # Límite superior
-print(f"\nLista de números del {n} al {m}:\n")
-for i in range(n, m):
-    print(i, end=", ")
-print(m)
